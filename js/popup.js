32855a9a423dc9ce16be4aa25d2efe72fb03894a
Dann <zadanfaidelta@gmail.com>
2023-12-20 22:53:00 +0700

    changing the popup kritik form

--- js/popup.js
+++ js/popup.js
@@ -3,9 +3,11 @@
 document.querySelector("#show-form").
 addEventListener("click", function(){
     document.querySelector(".popup").classList.add("active");
+    document.querySelector(".overlay").classList.add("active");
 });
 
 document.querySelector(".popup .close-button").
 addEventListener("click", function(){
     document.querySelector(".popup").classList.remove("active");
+    document.querySelector(".overlay").classList.remove("active");
 });
